267baf29d7ed084a30c2f72e3a4d935aa46fe191
sergeykin <sergeykinn@mail.ru>
2021-04-17 19:44:49 +0300

    4.1. JSON формат. [#456695]

--- src/main/java/ru/job4j/servlets/GreetingServlet.java
+++ src/main/java/ru/job4j/servlets/GreetingServlet.java
@@ -1,5 +1,7 @@
 package ru.job4j.servlets;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,14 +13,16 @@ public class GreetingServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/plain");
+        resp.setContentType("json");
         resp.setCharacterEncoding("UTF-8");
         String name = req.getParameter("name");
         resp.setHeader("Access-Control-Allow-Origin", "*");
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.println("{text : Nice to meet you, " + name+"}");
+        String str = "Nice to meet you, " + name;
 
-
+        ObjectMapper mapper = new ObjectMapper();
+        String string = mapper.writeValueAsString(str);
+        writer.write(string);
         writer.flush();
     }
 }
